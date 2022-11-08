1532f1590fb6dcaa4f24e4c91f8676bfacb7cf0d
unknown <deekshabisht16@gmail.com>
2022-11-08 10:29:23 +0530

    card details page done

--- src/components/CardsDetails.js
+++ src/components/CardsDetails.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from "react";
+import Table from 'react-bootstrap/Table'
 
-export default function CardDetails(){
-    return(
-        <div></div>
-    )
+export default function CardDetails() {
+  return (
+    <>
+      <div className="container mt-2">
+        <h2 className="text-center">Items Details Page</h2>
+        <section className="container mt-3">
+          <div className="itemsdetails">
+            <div className="items_img">
+              <img src="https://b.zmtcdn.com/data/pictures/9/18857339/8f53919f1175c08cf0f0371b73704f9b_o2_featured_v2.jpg?output-format=webp" alt="" />
+            </div>
+            <div className="details"></div>
+            <Table>
+            <tr>
+            <td>
+            <p><strong>Restaurant</strong> : Massala Theory </p>
+            <p><strong>Price</strong> : ₹ 300 </p>
+            <p><strong>Dishes</strong> : North Indian, Biryani, Mughlai</p>
+            <p><strong>Total</strong> : ₹ 300 </p>
+            </td>
+            <td>
+            <p><strong>Rating :</strong> <span style={{background:"green",color:"#fff",padding:"2px 5px",borderRadius:"5px"}}> 3.5 ★</span> </p>
+            <p><strong>Order Review</strong> <span>1175 + order placed from here recently</span> </p>
+            <p><strong> Remove :</strong> <span><i className="fas fa-trash" style={{color:"red",fontSize:20,cursor:"pointer"}}></i></span> </p>
+            </td>
+            </tr>
+            </Table>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 }
